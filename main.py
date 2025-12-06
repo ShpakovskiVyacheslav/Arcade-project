d97d47332bfb68d08a97989cea27ba5ac84a5242
Dozh <meser3642@gmail.com>
2025-12-06 16:47:20 +0300

    Сделал вторую версию меню

--- main.py
+++ main.py
@@ -12,15 +12,38 @@ class Fish_hunter_menu(arcade.View):
         super().__init__()
         self.ui_manager = UIManager()
 
+        style = {
+            "normal": {
+                "font_name": ("calibri", "arial"),
+                "font_size": 15,
+                "font_color": arcade.color.BLACK_OLIVE,
+                "bg": arcade.color.LIGHT_BLUE,
+                "border": arcade.color.BLACK_OLIVE,
+                "border_width": 2
+            },
+            "hover": {"font_name": ("calibri", "arial"),
+                      "font_size": 15,
+                      "font_color": arcade.color.LIGHT_BLUE,
+                      "bg": arcade.color.BLACK_OLIVE,
+                      "border": arcade.color.BLACK_OLIVE,
+                      "border_width": 2},
+            "press": {"font_name": ("calibri", "arial"),
+                      "font_size": 15,
+                      "font_color": arcade.color.LIGHT_BLUE,
+                      "bg": arcade.color.BLACK_OLIVE,
+                      "border": arcade.color.BLACK_OLIVE,
+                      "border_width": 2}
+        }
+
         # Создаем кнопку "Играть"
-        play_button = UIFlatButton(text="Играть", width=100)
+        play_button = UIFlatButton(text="Играть", width=300, style=style)
         play_button.on_click = self.start_game
         self.ui_manager.add(play_button)
         play_button.center_x = SCREEN_WIDTH // 2
         play_button.center_y = SCREEN_HEIGHT // 2
 
         # Создаем кнопку "Выход"
-        quit_button = UIFlatButton(text="Выход", width=100)
+        quit_button = UIFlatButton(text="Выход", width=300, style=style)
         quit_button.on_click = self.quit_game
         self.ui_manager.add(quit_button)
         quit_button.center_x = SCREEN_WIDTH // 2
@@ -30,7 +53,7 @@ class Fish_hunter_menu(arcade.View):
     def on_show_view(self):
         # Активируем менеджер UI
         self.ui_manager.enable()
-        arcade.set_background_color(arcade.color.WHITE)
+        arcade.set_background_color(arcade.color.LIGHT_BLUE)
 
     def on_hide_view(self):
         # Деактивируем менеджер UI
@@ -39,7 +62,7 @@ class Fish_hunter_menu(arcade.View):
     def on_draw(self):
         self.clear()
         # Рисуем все элементы
-        arcade.draw_text("Fish hunter", 300, 500, arcade.color.AMBER, 24)
+        arcade.draw_text("Fish hunter", 330, 500, arcade.color.BLACK_OLIVE, 24)
         self.ui_manager.draw()
 
     def start_game(self, event):
@@ -48,6 +71,7 @@ class Fish_hunter_menu(arcade.View):
 
     def quit_game(self, event):
         arcade.exit()
+
 
 class Fish_hunter_game(arcade.View):
     def __init__(self):
