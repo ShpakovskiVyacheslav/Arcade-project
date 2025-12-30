293a4b9282d4d5fc26f3dc449930d5e8d1f4f333
Dozh <meser3642@gmail.com>
2025-12-30 17:44:27 +0300

    Сделал адекватный физ движок, камеру, тестовый уровень и починил анимацию Потапа

--- character.py
+++ character.py
@@ -1,27 +1,28 @@
 import arcade
-import enum
 
-# Делаем класс для направления взгляда персонажа,
-class FaceDirection(enum.Enum):
-    LEFT = 0
-    RIGHT = 1
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "Fish_hunter"
-PLATFORM_TOP = 60
+CAMERA_LERP = 0.12
+GRAVITY = 0.5
 
-class Potap(arcade.Sprite):
+
+class Player_Potap(arcade.Sprite):
     def __init__(self):
         super().__init__()
 
         # Основные характеристики
-        self.scale = 3.0
-        self.speed = 300
+        self.scale = 1  # если поставьте больше 1 работать будет криво
+        self.speed = 5
+
+        # Физические параметры
+        self.dx = 0
+        self.dy = 0
+        self.jump_speed = 15
 
         # Загрузка текстур
-        self.idle_texture = arcade.load_texture(
-            "static/images/cat/Cat_stand.png",)
+        self.idle_texture = arcade.load_texture("static/images/cat/Cat_stand.png")
         self.texture = self.idle_texture
 
         self.walk_textures = []
@@ -34,107 +35,83 @@ class Potap(arcade.Sprite):
             texture = arcade.load_texture(f"static/images/cat/Cat_jump_{i}.png")
             self.jump_textures.append(texture)
 
-        self.current_texture = 0
+        self.x_frame_animation = 0
         self.texture_change_time = 0
         self.texture_change_delay = 0.1  # секунд на кадр
+        self.jump_time = 0
+        self.jump_time_animation = 0.5
+        self.y_frame_animation = 0
 
-        self.is_walking_x = False
-        self.is_walking_y = False
-        self.face_direction = FaceDirection.RIGHT
+        self.is_walking = False
+        self.is_jumping = False
+        self.face_direction = True
 
-        self.center_x = SCREEN_WIDTH // 2
-        self.center_y = SCREEN_HEIGHT // 2
-
-        self.dx, self.dy = 0, 0
+        # Начальная позиция
+        self.center_x = 100
+        self.center_y = 400  # Над платформой
 
     def update_animation(self, delta_time: float = 1 / 60):
         # Обновление анимации
-        if self.is_walking_y:
-            if self.dy > 10:
-                if self.face_direction == FaceDirection.RIGHT:
-                    self.texture = self.jump_textures[0]
-                else:
-                    self.texture = self.jump_textures[0].flip_horizontally()
-            elif self.dy > 5:
-                if self.face_direction == FaceDirection.RIGHT:
-                    self.texture = self.jump_textures[1]
-                else:
-                    self.texture = self.jump_textures[1].flip_horizontally()
-            if self.dy > 1:
-                if self.face_direction == FaceDirection.RIGHT:
-                    self.texture = self.jump_textures[2]
-                else:
-                    self.texture = self.jump_textures[2].flip_horizontally()
-            elif self.dy < 10:
-                if self.face_direction == FaceDirection.RIGHT:
-                    self.texture = self.jump_textures[4]
-                else:
-                    self.texture = self.jump_textures[4].flip_horizontally()
-            elif self.dy < 1:
-                if self.face_direction == FaceDirection.RIGHT:
-                    self.texture = self.jump_textures[3]
-                else:
-                    self.texture = self.jump_textures[3].flip_horizontally()
+        if self.is_jumping:
+            # Анимация прыжка в зависимости от скорости по Y
+            if self.change_y > 10:
+                texture_index = 0
+            elif self.change_y > 0:
+                texture_index = 2
+            elif self.change_y < -1:
+                texture_index = 4
+            elif self.change_y < -10:
+                texture_index = 5
             else:
-                # Если не идём, то просто показываем текстуру покоя
-                # и поворачиваем её в зависимости от направления взгляда
-                if self.face_direction == FaceDirection.RIGHT:
-                    self.texture = self.idle_texture
-                else:
-                    self.texture = self.idle_texture.flip_horizontally()
+                texture_index = 4
 
-        elif self.is_walking_x:
+            if self.face_direction:
+                self.texture = self.jump_textures[texture_index]
+            else:
+                self.texture = self.jump_textures[texture_index].flip_horizontally()
+
+
+        elif self.is_walking:
+            # Анимация ходьбы
             self.texture_change_time += delta_time
             if self.texture_change_time >= self.texture_change_delay:
                 self.texture_change_time = 0
-                self.current_texture += 1
-                if self.current_texture >= len(self.walk_textures):
-                    self.current_texture = 0
-                # Поворачиваем текстуру в зависимости от направления взгляда
-                if self.face_direction == FaceDirection.RIGHT:
-                    self.texture = self.walk_textures[self.current_texture]
-                else:
-                    self.texture = self.walk_textures[self.current_texture].flip_horizontally()
+                self.x_frame_animation += 1
+                if self.x_frame_animation >= len(self.walk_textures):
+                    self.x_frame_animation = 0
 
+                if self.face_direction:
+                    self.texture = self.walk_textures[self.x_frame_animation]
+                else:
+                    self.texture = self.walk_textures[self.x_frame_animation].flip_horizontally()
         else:
-            # Если не идём, то просто показываем текстуру покоя
-            # и поворачиваем её в зависимости от направления взгляда
-            if self.face_direction == FaceDirection.RIGHT:
+            # Анимация покоя
+            if self.face_direction:
                 self.texture = self.idle_texture
             else:
                 self.texture = self.idle_texture.flip_horizontally()
 
-
-    def update(self, delta_time, keys_pressed):
-        # Перемещение персонажа
-        # В зависимости от нажатых клавиш определяем направление движения
-
-        self.dy -= 0.5
-        if arcade.key.LEFT in keys_pressed or arcade.key.A in keys_pressed:
-            self.dx -= self.speed * delta_time
-        if arcade.key.RIGHT in keys_pressed or arcade.key.D in keys_pressed:
-            self.dx += self.speed * delta_time
-        elif arcade.key.SPACE in keys_pressed:
-            # Прыгаем только если стоим на платформе
-            if self.dy == 0:
-                self.dy = 15  # скорость прыжка
-
-        if self.dx != 0 and self.dy != 0:
-            factor = 0.7071
-            self.dx *= factor
-            self.dy *= factor
+    def update_movement(self):
+        # Перемещение
         self.center_x += self.dx
         self.center_y += self.dy
-        # Поворачиваем персонажа в зависимости от направления движения
-        if self.dx < 0:
-            self.face_direction = FaceDirection.LEFT
-        elif self.dx > 0:
-            self.face_direction = FaceDirection.RIGHT
 
-        # Ограничение в пределах экрана
-        self.center_x = max(self.width / 2, min(SCREEN_WIDTH - self.width / 2, self.center_x))
-        self.center_y = max(self.height / 2, min(SCREEN_HEIGHT - self.height / 2, self.center_y))
+        # Определение состояния для анимации
+        self.is_walking = abs(self.dx) > 0.1
+        self.is_jumping = self.change_y != 0
 
-        # Проверка на движение
-        self.is_walking_x = self.dx
-        self.is_walking_y = self.dy
+    def move_left(self):
+        self.dx = -self.speed
+        self.face_direction = False
+
+    def move_right(self):
+        self.dx = self.speed
+        self.face_direction = True
+
+    def stop_horizontal(self):
+        self.dx = 0
+
+    def jump(self):
+        # Прыжок возможен только если персонаж стоит на платформе
+        #self.dy = self.jump_speed
+        self.is_jumping = True
--- game.py
+++ game.py
@@ -1,46 +1,85 @@
 import arcade
-from character import Potap
+from character import Player_Potap, SCREEN_WIDTH, SCREEN_HEIGHT, CAMERA_LERP, GRAVITY
+from arcade.camera import Camera2D
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-SCREEN_TITLE = "Fish_hunter"
-PLATFORM_TOP = 60
+TILE_SCALING = 1
 
 class Fish_hunter_game(arcade.View):
     def __init__(self):
         super().__init__()
         arcade.set_background_color(arcade.color.LIGHT_BLUE)
+        self.world_camera = Camera2D()
+        self.gui_camera = Camera2D()
 
-        # Позиция и скорость кубика
-        self.players = arcade.SpriteList()
-        self.cat_potap = Potap()
-        self.players.append(self.cat_potap)
+        # Создаем игрока
+        self.all_sprite = arcade.SpriteList()
+        self.player = Player_Potap()
+        self.all_sprite.append(self.player)
 
-        self.keys_pressed = set()
-
-        # Состояние клавиш
+        # Управление
         self.left_pressed = False
         self.right_pressed = False
 
+        map_name = "test.tmx"
+        self.tile_map = arcade.load_tilemap(f"static/levels/{map_name}", scaling=TILE_SCALING)
+        self.scene = arcade.Scene.from_tilemap(self.tile_map)
+
+        self.physics_engine = arcade.PhysicsEnginePlatformer(player_sprite=self.player,
+                                                             platforms=self.scene["collision"],
+                                                             gravity_constant=GRAVITY)
+
     def on_draw(self):
         self.clear()
+        # камера
+        self.world_camera.use()
+        self.scene.draw()
+        self.all_sprite.draw()
+        self.gui_camera.use()
 
-        # Рисуем платформу
-
-
-        # Кота Потапа (Игрока)
-        self.players.draw()
-        self.players.draw_hit_boxes()
+        # Рисуем игрока
 
     def on_update(self, delta_time):
-        self.players[0].update(delta_time, self.keys_pressed)
-        # Обновляем анимации игрока
-        self.players[0].update_animation()
+        # Обновляем движение
+        if self.left_pressed and not self.right_pressed:
+            self.player.move_left()
+        elif self.right_pressed and not self.left_pressed:
+            self.player.move_right()
+        else:
+            self.player.stop_horizontal()
+
+        # Обновляем физику и движение
+        self.physics_engine.update()
+        self.player.update_movement()
+
+        # Обновляем анимацию
+        self.player.update_animation(delta_time)
+
+        tx, ty = self.player.center_x, self.player.center_y
+        cx, cy = self.world_camera.position
+        smooth = (cx + (tx - cx) * CAMERA_LERP,
+                  cy + (ty - cy) * CAMERA_LERP)
+
+        half_w = self.world_camera.viewport_width / 2
+        half_h = self.world_camera.viewport_height / 2
+
+        world_w = 4000
+        world_h = 4000
+        cam_x = max(half_w, min(world_w - half_w, smooth[0]))
+        cam_y = max(half_h, min(world_h - half_h, smooth[1]))
+        self.world_camera.position = (cam_x, cam_y)
+        self.gui_camera.position = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     def on_key_press(self, key, modifiers):
-        self.keys_pressed.add(key)
+        if key == arcade.key.LEFT or key == arcade.key.A:
+            self.left_pressed = True
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.right_pressed = True
+        elif key == arcade.key.SPACE and self.physics_engine.can_jump(y_distance=1):
+            self.player.jump()
+            self.physics_engine.jump(15)
 
     def on_key_release(self, key, modifiers):
-        if key in self.keys_pressed:
-            self.keys_pressed.remove(key)
-            self.cat_potap.dx = 0
+        if key == arcade.key.LEFT or key == arcade.key.A:
+            self.left_pressed = False
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.right_pressed = False
